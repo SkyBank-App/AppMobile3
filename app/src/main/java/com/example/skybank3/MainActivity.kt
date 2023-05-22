48f9d879660430b60fe14c7da70becd17bc3f8c1
carlo <20220367@ricaldone.edu.sv>
2023-05-22 00:02:28 -0600

    Navegavilidad de tres pantallas de Usuario

--- app/src/main/java/com/example/skybank3/MainActivity.kt
+++ app/src/main/java/com/example/skybank3/MainActivity.kt
@@ -37,7 +37,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_home, R.id.idAgendarCitas, R.id.estadoCuenta, R.id.idTransferencias
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
